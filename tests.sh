efa87532272ada6975b29385581cc85f8fe8d982
Josh Marshall <jrmarsha@mtu.edu>
2015-09-11 02:37:53 -0400

    This is the first semi-functional commit.  By running tests.sh now, it will automatically perform 100 tests of each program over each test input file printing out programs and file names as they change, and run information is printed at the number of executed instructions on the CPU and the peak memory usage in KB as seperated columns, with each row being a distinct run.  Changes to the README are incoming.

--- tests.sh
+++ tests.sh
@@ -1,2 +1,16 @@
-/usr/bin/time -v ./timeTest input-files/chr22.dna
+#!/bin/bash
+##
 
+for i in bin/* ; do
+  [[ -e $i ]] || break
+  echo "$i"
+  for j in input-files/* ; do
+    [[ -e $j ]] || break
+    echo "$j"
+    for h in {1..100} ; do
+      /usr/bin/time -f %M perf stat -e instructions "$i" "$j" |& sed ':a;N;$!ba;s/\n/ /g' | sed -e 's/\s/ /g' | xargs | awk '{ print $7 " " $13 }'
+    done
+    echo ""
+  done
+done 
+rm perf.data
