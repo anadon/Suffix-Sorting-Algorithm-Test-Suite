b4edbf15ba74cd6fd2321e9fa4425da738b1d0cf
Josh Marshall <jrmarsha@mtu.edu>
2015-09-12 13:20:27 -0400

    improved test scripts, more usable with gnuplot, and starting to get sais and linduvsufsort to work.

--- tests.sh
+++ tests.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 ##
 
+mkdir results
+
 for i in bin/* ; do
   [[ -e $i ]] || break
   echo "$i"
   for j in input-files/* ; do
     [[ -e $j ]] || break
     echo "$j"
-    for h in {1..100} ; do
-      /usr/bin/time -f %M perf stat -e instructions "$i" "$j" |& sed ':a;N;$!ba;s/\n/ /g' | sed -e 's/\s/ /g' | xargs | awk '{ print $7 " " $13 }'
-    done
-    echo ""
+    if [ ! \( -e "results/$(basename "$i")-$(basename "$j").dat" \) ] ; then
+      rm -f "results/$(basename "$i")-$(basename "$j").dat"
+      for h in {1..100} ; do
+        /usr/bin/time -f %M perf stat -e instructions "$i" "$j"  |& \
+        sed ':a;N;$!ba;s/\n/ /g' | sed -e 's/\s/ /g' | sed 's/,//g' | xargs | \
+        awk '{ print $7 " " $13 }' &>> "results/$(basename "$i")-$(basename "$j").dat"
+      done
+    fi
   done
-done 
-rm perf.data
+done
