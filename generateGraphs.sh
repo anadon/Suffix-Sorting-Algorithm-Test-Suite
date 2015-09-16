996b786edc7dd94d5c03626a7c3e2f5390b4dd36
Josh Marshall <jrmarsha@mtu.edu>
2015-09-16 14:40:37 -0400

    Trying to fix generated graphs not outputting.

--- generateGraphs.sh
+++ generateGraphs.sh
@@ -5,6 +5,6 @@ cd results || exit
 mkdir graphs &> /dev/null
 for i in * ; do
   cd "$i" || exit
-  gnuplot -e "set title '"$i"' ; set output '../graphs/"$i".svg'" -c ../../generateGraphs.gp
+  gnuplot -e "set title '"$i"' ; set term svg ; set output '../graphs/"$i".svg'" -c ../../generateGraphs.gp
   cd ..
 done
